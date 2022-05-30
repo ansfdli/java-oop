1c5da6df6c8b763e63e74c7dcf2992934a91f3ac
ansfdli <anasfadli@outlook.com>
2022-05-30 21:21:00 +0700

    java oop

---
+++ src/testing/oop/app/CategoryApp.java
@@ -0,0 +1,15 @@
+package testing.oop.app;
+
+import testing.oop.data.Category;
+
+public class CategoryApp {
+  public static void main(String[] args) {
+
+
+    Category category = new Category();
+    category.setId("ID");
+    category.setId(null);
+
+    System.out.println(category.getId());
+  }
+}
